4e06ad1de5ed02c3beee5f741462d0b01da6a123
MasoudSRad <amir.sepehr72@gmail.com>
2022-09-04 19:24:38 +0430

    font and more styles added

--- scripts.js
+++ scripts.js
@@ -8,14 +8,16 @@ let sec = 0;
 let min = 0;
 let timerInProgress = false;
 let startTimer;
-let timeListHeight = 80
+let timeListHeight = 80;
 
 const startPauseHandler = () => {
 	if (!timerInProgress) {
 		startPauseButton.innerHTML = "Pause";
-		startPauseButton.style.width="350px"
+		startPauseButton.style.width = "350px";
 		setButton.style.display = "inline";
-		document.querySelector(".buttons").style.height="78px"
+		document.querySelector(".buttons").style.height = "78px";
+		document.querySelector(".msecspinner").style.borderTop = "16px solid #db345b";
+		document.querySelector(".secspinner").style.borderTop = "16px solid #3498db";
 		startTimer = setInterval(() => {
 			msec++;
 			document.querySelector(".msec").innerHTML = msec;
@@ -38,7 +40,9 @@ const startPauseHandler = () => {
 		timerInProgress = false;
 		startPauseButton.innerHTML = "Resume";
 		resetButton.style.display = "block";
-		document.querySelector(".buttons").style.height="160px"
+		document.querySelector(".buttons").style.height = "160px";
+		document.querySelector(".msecspinner").style.borderTop = "16px solid #f3f3f3";
+		document.querySelector(".secspinner").style.borderTop = "16px solid #f3f3f3";
 	}
 };
 
@@ -47,18 +51,18 @@ const resetHandler = () => {
 	document.querySelector(".msec").innerHTML = "00";
 	document.querySelector(".sec").innerHTML = "00";
 	document.querySelector(".min").innerHTML = "00";
-	startPauseButton.style.width="700px"
-	setButton.style.display="none"
-	document.querySelector(".buttons").style.height="78px"
-	startPauseButton.innerHTML="Start"
+	startPauseButton.style.width = "700px";
+	setButton.style.display = "none";
+	document.querySelector(".buttons").style.height = "78px";
+	startPauseButton.innerHTML = "Start";
 };
 
 const setHandler = () => {
-	clearButton.style.display="inline"
+	clearButton.style.display = "inline";
 	const timeList = document.querySelector(".timeList");
 	timeList.style.display = "flex";
-	timeListHeight += 77
-	timeList.style.height = `${timeListHeight}px`
+	timeListHeight += 77;
+	timeList.style.height = `${timeListHeight}px`;
 	const timeListUL = document.querySelector(".timeList ul");
 	const li = document.createElement("li");
 	li.innerHTML = `${min}:${sec}:${msec}`;
@@ -68,7 +72,7 @@ const setHandler = () => {
 const clearHandler = () => {
 	const timeList = document.querySelector(".timeList");
 	timeList.style.height = "0";
-	timeListHeight = 80
+	timeListHeight = 80;
 	const timeListUL = document.querySelector(".timeList ul");
 	timeListUL.innerHTML = "";
 };
